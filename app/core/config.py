b419f099b490d926514cd5253b2ebd9e9321489b
Fatih Aydin <bozok17@gmail.com>
2025-04-28 16:39:02 +0300

    Update config.py
    fix pydantic v1 issue

--- app/core/config.py
+++ app/core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
     # CORS settings (adjust as needed)
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
-    @field_validator("BACKEND_CORS_ORIGINS", pre=True)
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
